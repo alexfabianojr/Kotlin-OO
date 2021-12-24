d0eb67354b9682e0988f2a43328eeb1a3ce2bb83
alex.junior <alex.junior@consulting-for.edenred.com>
2021-12-24 18:32:57 -0300

    inheritance 2

--- src/main/kotlin/Inheritance.kt
+++ src/main/kotlin/Inheritance.kt
@@ -4,6 +4,8 @@ open class Person {}
 
 class Warrior: Person() {}
 
+data class SomeDataType(val something: String) {}
+
 abstract class Printer(val model: String) {
     open fun printModel() = println("model: $model")
     abstract fun bestSellingPrice(): Double
@@ -15,4 +17,29 @@ class laserPrinter(model:String): Printer(model) {
     override fun bestSellingPrice(): Double =120.0
     //With final keyword no1 who extends laserPrinter can override it
     final override fun someThing() = super.someThing()
+}
+
+//interfaces are extendable by default
+
+interface MyInterface {
+    val number: Int
+        get() = 50
+
+    fun someFun1()
+}
+
+interface MySecondInterface: MyInterface {
+    fun someFun2()
+}
+
+class WillImplementInterface: MySecondInterface {
+    override fun someFun2() {
+        TODO("Not yet implemented")
+    }
+
+    override fun someFun1() {
+        TODO("Not yet implemented")
+    }
+
+    fun showSomething() = println(number) //from MyInterface
 }
